61a8b08213ab0327182b787eb1d8d3392c2bb700
Skoulskiy <ogurethcs@gmail.com>
2026-07-06 14:16:35 +0300

    added fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@
 function waitFor(element, eventName) {
   return new Promise((resolve) => {
     element.addEventListener(eventName, (e) => {
-      resolve(
-        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`,
+      printMessage(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
       );
     });
   });
